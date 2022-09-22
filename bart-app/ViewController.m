a3c2378ad663e5db2b5753ed9705cf7d75bb3875
John Anderson <john@rhombussystems.com>
2022-09-22 10:27:49 -0700

    Initial Push

--- bart-app/ViewController.m
+++ bart-app/ViewController.m
@@ -7,6 +7,9 @@
 
 #import "ViewController.h"
 
+static NSString * GET_STATIONS =   @"https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y";
+static NSString * GET_DEPARTURES_FMT = @"https://api.bart.gov/api/etd.aspx?cmd=etd&orig=%@";
+
 @interface ViewController ()
 
 @end
@@ -15,7 +18,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    
+    NSURL *url = [NSURL URLWithString:GET_STATIONS];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+        NSError *jsonError;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+        NSLog(@"Got response %@", json);
+    }];
+    
+    [task resume];
+
 }
 
 
